124f9b650d843604d41c299ba799934000e395f9
kohkimakimoto <kohki.makimoto@gmail.com>
2015-12-21 14:13:44 +0900

    update

---
+++ _example/example.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"github.com/kohkimakimoto/cli"
+	"os"
+	"fmt"
+)
+
+func main() {
+	CLI := cli.NewCLI()
+	CLI.Name = "example"
+	CLI.Commands = []cli.Command{
+		cli.Command{
+			Name: "hello",
+			Action: func(ctx *cli.Context) error {
+				fmt.Println("Hello world")
+				return nil
+			},
+		},
+	}
+
+	err := CLI.Run(os.Args)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Got a error: %\n", err)
+		os.Exit(1)
+	}
+}
